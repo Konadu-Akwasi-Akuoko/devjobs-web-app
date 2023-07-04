82b1b72d8b337b17bda18223e7c7ad912a4d678c
Konadu Akwasi Akuoko <konaduakwasiakuoko@gmail.com>
2023-07-04 00:13:54 +0000

    Added typography css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,13 @@
 import Image from "next/image";
 
 export default function Home() {
-  return <main>Hello it's me</main>;
+  return (
+    <main>
+      <h1>Hello it's me</h1>
+      <h2>Hello it's me</h2>
+      <h3>Hello it's me</h3>
+      <h4>Hello it's me</h4>
+      <p>Hello it's me</p>
+    </main>
+  );
 }
