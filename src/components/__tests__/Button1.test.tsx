189790ad05080f51f3a8445738ec326d399b1af4
Konadu Akwasi Akuoko <konaduakwasiakuoko@gmail.com>
2023-07-04 00:57:48 +0000

    Set up testing for nextjs react

---
+++ src/components/__tests__/Button1.test.tsx
@@ -0,0 +1,7 @@
+import { render } from "react-dom";
+import Button1 from "../Button1";
+
+describe("Button1", () => {
+  // Test 1
+  it("should render a button with the given text", () => {});
+});
