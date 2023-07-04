a3007418c6e6b9ccdfe7c04f2cc8031d5e9a0319
Konadu Akwasi Akuoko <konaduakwasiakuoko@gmail.com>
2023-07-04 00:00:33 +0000

    Extended the color pallete to include the project colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        primary: {
+          violet: "#5964E0",
+          "light-violet": "#939BF4",
+          "very-dark-blue": "#19202D",
+          midnight: "#121721",
+        },
+        secondary: {
+          white: "#FFFFFF",
+          "light-gray": "#F4F6F8",
+          gray: "#9DAEC2",
+          "dark-gray": "#6E8098",
+        },
       },
     },
   },
   plugins: [],
-}
+};
