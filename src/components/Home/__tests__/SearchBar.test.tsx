6ec03b6798c2fd7e351225f77e9cb0447550b157
Konadu Akwasi Akuoko <konaduakwasiakuoko@gmail.com>
2023-07-07 16:47:39 +0000

    Update the searchBar test file to include clicking mouse events and fireing keyboard events

--- src/components/Home/__tests__/SearchBar.test.tsx
+++ src/components/Home/__tests__/SearchBar.test.tsx
@@ -1,7 +1,13 @@
 import "@testing-library/jest-dom/extend-expect";
-import { render, within } from "@testing-library/react";
+import { render, within, fireEvent } from "@testing-library/react";
 import SearchBar from "../SearchBar";
 import StoreProvider from "@/components/StoreProvider";
+import { store } from "@/store/store";
+import {
+  selectCompanyState,
+  selectIsFullTimeState,
+  selectLocationState,
+} from "@/store/searchSlice";
 
 describe("SearchBar", () => {
   it("Should render the search bar correctly", () => {
@@ -13,5 +19,45 @@ describe("SearchBar", () => {
 
     within(container).getByTestId("button1");
     within(container).getAllByTestId("form-input");
+  });
+
+  it("Should update the company state when typing in the company input", () => {
+    const { container } = render(
+      <StoreProvider>
+        <SearchBar />
+      </StoreProvider>
+    );
+
+    const companyInput = within(container).getByPlaceholderText(
+      "Filter by title, companies, expertise…"
+    );
+    fireEvent.change(companyInput, { target: { value: "Amalitech" } });
+    expect(selectCompanyState(store.getState())).toBe("Amalitech");
+  });
+
+  it("Should update the location state when typing in the location input", () => {
+    const { container } = render(
+      <StoreProvider>
+        <SearchBar />
+      </StoreProvider>
+    );
+
+    const locationInput = within(container).getByPlaceholderText(
+      "Filter by location…"
+    );
+    fireEvent.change(locationInput, { target: { value: "Accra" } });
+    expect(selectLocationState(store.getState())).toBe("Accra");
+  });
+
+  it("Should update the isFullTime state when clicking on the checkbox", () => {
+    const { container } = render(
+      <StoreProvider>
+        <SearchBar />
+      </StoreProvider>
+    );
+
+    const checkBox = within(container).getByTestId("checkbox");
+    fireEvent.click(checkBox);
+    expect(selectIsFullTimeState(store.getState())).toBe(true);
   });
 });
