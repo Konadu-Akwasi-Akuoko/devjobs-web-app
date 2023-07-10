930d7fd2fae03cd7795757ba6b8c10d0b5330585
Konadu Akwasi Akuoko <konaduakwasiakuoko@gmail.com>
2023-07-10 15:13:33 +0000

    Made changes in the test for the FilterButton

--- src/components/Home/__tests__/FilterButton.test.tsx
+++ src/components/Home/__tests__/FilterButton.test.tsx
@@ -1,15 +1,14 @@
 import "@testing-library/jest-dom/extend-expect";
 import { render, screen, fireEvent } from "@testing-library/react";
-import { Provider } from "react-redux";
 import FilterButton from "../FilterButton";
-import { store } from "@/store/store";
+import StoreProvider from "@/components/StoreProvider";
 
 describe("FilterButton", () => {
   it("should render the filter button and the modal correctly", () => {
     render(
-      <Provider store={store}>
+      <StoreProvider>
         <FilterButton />
-      </Provider>
+      </StoreProvider>
     );
 
     expect(screen.getByRole("button")).toBeVisible();
