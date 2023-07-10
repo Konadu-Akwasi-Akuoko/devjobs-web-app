c39b087ce9f02d83f417ac1bac01111d4250becd
Konadu Akwasi Akuoko <konaduakwasiakuoko@gmail.com>
2023-07-10 15:17:22 +0000

    Created a new card, and removed all unneccosory import from @/app/page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,11 @@
-import Button1 from "@/components/Button1";
-import Button2 from "@/components/Button2";
-import FormInput from "@/components/FormInput";
-import searchIcon from "@/assets/desktop/icon-search.svg";
-import CheckBox from "@/components/Checkbox";
-import ThemeToggle from "@/components/ThemeToggle";
 import SearchBar from "@/components/Home/SearchBar";
+import JobCard from "@/components/Home/JobCard";
 
 export default function Home() {
   return (
     <main>
       <SearchBar />
+      <JobCard />
     </main>
   );
 }
