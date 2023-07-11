aa67094909b3a2d6d392d5d0087182d34769c258
Konadu Akwasi Akuoko <konaduakwasiakuoko@gmail.com>
2023-07-11 18:08:19 +0000

    Creaed the heading for the DataFromID page

--- src/components/Jobs/ID/DataFromID.tsx
+++ src/components/Jobs/ID/DataFromID.tsx
@@ -1,4 +1,6 @@
+/* eslint-disable @next/next/no-img-element */
 "use client";
+import Button2 from "@/components/Button2";
 import {
   loadClickedPost,
   loadInitialPosts,
@@ -24,9 +26,39 @@ export default function DataFromID({
       await store.dispatch(loadInitialPosts(data));
     }
     await store.dispatch(loadClickedPost(Number(id)));
-    console.log(clickedPostData);
   });
   return (
-    <section className="horizontal-padding">{clickedPostData?.company}</section>
+    <section className="horizontal-padding">
+      {/* Header */}
+      <div className="flex flex-row rounded-md overflow-hidden -mt-10  bg-white dark:bg-primary-very-dark-blue">
+        <div
+          className="w-[140px] h-[140px] overflow-hidden flex items-center justify-center"
+          style={{ backgroundColor: clickedPostData?.logoBackground }}
+        >
+          <img
+            src={`.${clickedPostData?.logo}`}
+            alt="logo"
+            className="w-3/4 h-auto"
+          />
+        </div>
+        <div className="ml-10 flex flex-row grow justify-between items-center">
+          <div>
+            <h1 className=" text-2xl text-primary-very-dark-blue dark:text-white font-bold mb-6">
+              {clickedPostData?.company}
+            </h1>
+            <p className="text-secondary-dark-gray">
+              {clickedPostData?.company.toLocaleLowerCase()}.com
+            </p>
+          </div>
+          <div className="mr-10">
+            <Button2>
+              <a href={clickedPostData?.website} target="_blank">
+                Company Site
+              </a>
+            </Button2>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 }
